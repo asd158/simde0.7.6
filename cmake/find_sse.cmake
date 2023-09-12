13d45043ffbc18924ecaa450f653d99bfe8dc93b
asd158 <gaowycn@qq.com>
2023-09-12 18:57:43 +0800

    合并avx& sse

--- cmake/find_sse.cmake
+++ cmake/find_sse.cmake
@@ -1,30 +1,28 @@
 ###############################################################################
 # Check for the presence of SSE and figure out the flags to use for it.
-function(CHECK_FOR_SSE)
-  SET(SSE_FLAGS)
-  SET(SSE_DEFINITIONS)
-  IF (NOT CMAKE_CROSSCOMPILING)
-    # Test GCC/G++ and CLANG
-    IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
-      INCLUDE(CheckCXXCompilerFlag)
-      CHECK_CXX_COMPILER_FLAG("-march=native" HAVE_MARCH)
+FUNCTION(CHECK_FOR_SSE)
+    SET(SSE_FLAGS)
+    SET(SSE_DEFINITIONS)
+    IF (NOT CMAKE_CROSSCOMPILING)
+        # Test GCC/G++ and CLANG
+        IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+            INCLUDE(CheckCXXCompilerFlag)
+            CHECK_CXX_COMPILER_FLAG("-march=native" HAVE_MARCH)
+        ENDIF ()
+    ELSE ()
+        RETURN()
     ENDIF ()
-  ELSE ()
-    RETURN()
-  ENDIF ()
 
-  # Unfortunately we need to check for SSE to enable "-mfpmath=sse" alongside
-  # "-march=native". The reason for this is that by default, 32bit architectures
-  # tend to use the x87 FPU (which has 80 bit internal precision), thus leading
-  # to different results than 64bit architectures which are using SSE2 (64 bit internal
-  # precision). One solution would be to use "-ffloat-store" on 32bit (see
-  # http://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html), but that slows code down,
-  # so the preferred solution is to try "-mpfmath=sse" first.
-  include(CheckCXXSourceRuns)
-  set(CMAKE_REQUIRED_FLAGS)
-  set(SSE_LEVEL 0)
+    # Unfortunately we need to check for SSE to enable "-mfpmath=sse" alongside
+    # "-march=native". The reason for this is that by default, 32bit architectures
+    # tend to use the x87 FPU (which has 80 bit internal precision), thus leading
+    # to different results than 64bit architectures which are using SSE2 (64 bit internal
+    # precision). One solution would be to use "-ffloat-store" on 32bit (see
+    # http://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html), but that slows code down,
+    # so the preferred solution is to try "-mpfmath=sse" first.
+    INCLUDE(CheckCXXSourceRuns)
 
-  check_cxx_source_runs("
+    CHECK_CXX_SOURCE_RUNS("
     // Intel compiler defines an incompatible _mm_malloc signature
     #if defined(__INTEL_COMPILER)
         #include <malloc.h>
@@ -36,22 +34,42 @@ function(CHECK_FOR_SSE)
       void* mem = _mm_malloc (100, 16);
       return 0;
     }"
-    HAVE_MM_MALLOC)
+                          HAVE_MM_MALLOC)
 
-  check_cxx_source_runs("
+    CHECK_CXX_SOURCE_RUNS("
     #include <stdlib.h>
     int main()
     {
       void* mem;
       return posix_memalign (&mem, 16, 100);
     }"
-    HAVE_POSIX_MEMALIGN)
-
-  if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
-    set(CMAKE_REQUIRED_FLAGS "-msse4.2")
-  endif()
-
-  check_cxx_source_runs("
+                          HAVE_POSIX_MEMALIGN)
+    ###############################################################################################################
+    CHECK_CXX_SOURCE_RUNS("
+    #include <immintrin.h>
+    int main()
+    {
+      __m256i a = {0};
+      a = _mm256_abs_epi16(a);
+      return 0;
+    }"
+                          HAVE_AVX2)
+    IF (NOT HAVE_AVX2)
+        CHECK_CXX_SOURCE_RUNS("
+      #include <immintrin.h>
+      int main()
+      {
+        __m256 a;
+        a = _mm256_set1_ps(0);
+        return 0;
+      }"
+                              HAVE_AVX)
+    ENDIF ()
+    ###############################################################################################################
+    IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+        SET(CMAKE_REQUIRED_FLAGS "-msse4.2")
+    ENDIF ()
+    CHECK_CXX_SOURCE_RUNS("
     #include <emmintrin.h>
     #include <nmmintrin.h>
     int main ()
@@ -69,18 +87,16 @@ function(CHECK_FOR_SSE)
       else
         return (1);
     }"
-    HAVE_SSE4_2_EXTENSIONS)
-        
-  if(HAVE_SSE4_2_EXTENSIONS)
-    set(SSE_LEVEL 4.2)
-  endif()
+                          HAVE_SSE4_2_EXTENSIONS)
 
-  if(SSE_LEVEL LESS 4.2)
-    if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
-      set(CMAKE_REQUIRED_FLAGS "-msse4.1")
-    endif()
+    IF (HAVE_SSE4_2_EXTENSIONS)
+        SET(SSE_LEVEL 4.2)
+    ELSEIF (SSE_LEVEL LESS 4.2)
+        IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+            SET(CMAKE_REQUIRED_FLAGS "-msse4.1")
+        ENDIF ()
 
-    check_cxx_source_runs("
+        CHECK_CXX_SOURCE_RUNS("
       #include <smmintrin.h>
       int main ()
       {
@@ -93,19 +109,19 @@ function(CHECK_FOR_SSE)
         _mm_storeu_ps (vals,b);
         return (0);
       }"
-      HAVE_SSE4_1_EXTENSIONS)
-      
-    if(HAVE_SSE4_1_EXTENSIONS)
-      set(SSE_LEVEL 4.1)
-    endif()
-  endif()
+                              HAVE_SSE4_1_EXTENSIONS)
 
-  if(SSE_LEVEL LESS 4.1)
-    if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
-      set(CMAKE_REQUIRED_FLAGS "-mssse3")
-    endif()
-    
-    check_cxx_source_runs("
+        IF (HAVE_SSE4_1_EXTENSIONS)
+            SET(SSE_LEVEL 4.1)
+        ENDIF ()
+    ENDIF ()
+
+    IF (SSE_LEVEL LESS 4.1)
+        IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+            SET(CMAKE_REQUIRED_FLAGS "-mssse3")
+        ENDIF ()
+
+        CHECK_CXX_SOURCE_RUNS("
       #include <tmmintrin.h>
       int main ()
       {
@@ -116,19 +132,19 @@ function(CHECK_FOR_SSE)
         _mm_storeu_si128 ((__m128i*)vals, b);
         return (0);
       }"
-      HAVE_SSSE3_EXTENSIONS)
+                              HAVE_SSSE3_EXTENSIONS)
 
-    if(HAVE_SSSE3_EXTENSIONS)
-      set(SSE_LEVEL 3.1)
-    endif()
-  endif()
+        IF (HAVE_SSSE3_EXTENSIONS)
+            SET(SSE_LEVEL 3.1)
+        ENDIF ()
+    ENDIF ()
 
-  if(SSE_LEVEL LESS 3.1)
-    if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
-      set(CMAKE_REQUIRED_FLAGS "-msse3")
-    endif()
+    IF (SSE_LEVEL LESS 3.1)
+        IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+            SET(CMAKE_REQUIRED_FLAGS "-msse3")
+        ENDIF ()
 
-    check_cxx_source_runs("
+        CHECK_CXX_SOURCE_RUNS("
       #include <pmmintrin.h>
       int main ()
       {
@@ -139,21 +155,21 @@ function(CHECK_FOR_SSE)
           _mm_storeu_pd (vals, b);
           return (0);
       }"
-      HAVE_SSE3_EXTENSIONS)
+                              HAVE_SSE3_EXTENSIONS)
 
-    if(HAVE_SSE3_EXTENSIONS)
-      set( SSE_LEVEL 3.0)
-    endif()
-  endif()
+        IF (HAVE_SSE3_EXTENSIONS)
+            SET(SSE_LEVEL 3.0)
+        ENDIF ()
+    ENDIF ()
 
-  if(SSE_LEVEL LESS 3.0)
-    if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
-      set(CMAKE_REQUIRED_FLAGS "-msse2")
-    elseif(MSVC AND NOT CMAKE_CL_64)
-      set(CMAKE_REQUIRED_FLAGS "/arch:SSE2")
-    endif()
+    IF (SSE_LEVEL LESS 3.0)
+        IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+            SET(CMAKE_REQUIRED_FLAGS "-msse2")
+        ELSEIF (MSVC AND NOT CMAKE_CL_64)
+            SET(CMAKE_REQUIRED_FLAGS "/arch:SSE2")
+        ENDIF ()
 
-    check_cxx_source_runs("
+        CHECK_CXX_SOURCE_RUNS("
       #include <emmintrin.h>
       int main ()
       {
@@ -164,21 +180,21 @@ function(CHECK_FOR_SSE)
           _mm_storeu_pd (vals,b);
           return (0);
       }"
-      HAVE_SSE2_EXTENSIONS)
+                              HAVE_SSE2_EXTENSIONS)
 
-    if(HAVE_SSE2_EXTENSIONS)
-      set(SSE_LEVEL 2.0)
-    endif()
-  endif()
+        IF (HAVE_SSE2_EXTENSIONS)
+            SET(SSE_LEVEL 2.0)
+        ENDIF ()
+    ENDIF ()
 
-  if(SSE_LEVEL LESS 2.0)
-    if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
-      set(CMAKE_REQUIRED_FLAGS "-msse")
-    elseif(MSVC AND NOT CMAKE_CL_64)
-      set(CMAKE_REQUIRED_FLAGS "/arch:SSE")
-    endif()
+    IF (SSE_LEVEL LESS 2.0)
+        IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+            SET(CMAKE_REQUIRED_FLAGS "-msse")
+        ELSEIF (MSVC AND NOT CMAKE_CL_64)
+            SET(CMAKE_REQUIRED_FLAGS "/arch:SSE")
+        ENDIF ()
 
-    check_cxx_source_runs("
+        CHECK_CXX_SOURCE_RUNS("
       #include <xmmintrin.h>
       int main ()
       {
@@ -190,72 +206,90 @@ function(CHECK_FOR_SSE)
           _mm_storeu_ps (vals,b);
           return (0);
       }"
-      HAVE_SSE_EXTENSIONS)
+                              HAVE_SSE_EXTENSIONS)
 
-    if(HAVE_SSE_EXTENSIONS)
-      set(SSE_LEVEL 1.0)
-    endif()
-  endif()
-  
-  if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
-    if(SSE_LEVEL GREATER_EQUAL 1.0)
-      if(SSE_LEVEL GREATER_EQUAL 4.2)
-        set(SSE_FLAGS "-msse4.2")
-      elseif(SSE_LEVEL GREATER_EQUAL 4.1)
-        set(SSE_FLAGS "-msse4.1")
-      elseif(SSE_LEVEL GREATER_EQUAL 3.1)
-        set(SSE_FLAGS "-msse3")
-      elseif(SSE_LEVEL GREATER_EQUAL 3.0)
-        set(SSE_FLAGS "-msse3")
-      elseif(SSE_LEVEL GREATER_EQUAL 2.0)
-        set(SSE_FLAGS "-msse2")
-      else()
-        set(SSE_FLAGS "-msse")
-      endif()
-      string(APPEND SSE_FLAGS " -mfpmath=sse")
-    else()
-      # Setting -ffloat-store to alleviate 32bit vs 64bit discrepancies on non-SSE
-      # platforms.
-      string(APPEND SSE_FLAGS " -ffloat-store")
-    endif()
-    
-    if(PCL_ENABLE_MARCHNATIVE AND (NOT CMAKE_CROSSCOMPILING))
-      if(HAVE_MARCH)
-          string(APPEND SSE_FLAGS " -march=native")
-      else()
-          string(APPEND SSE_FLAGS " -mtune=native")
-      endif()
-      message(STATUS "Using CPU native flags for SSE optimization: ${SSE_FLAGS}")
-    endif()
-  elseif(MSVC AND NOT CMAKE_SIZEOF_VOID_P)
-    if(SSE_LEVEL GREATER_EQUAL 2.0)
-      set( SSE_FLAGS "/arch:SSE2")
-    elseif(SSE_LEVEL GREATER_EQUAL 1.0)
-      set( SSE_FLAGS "/arch:SSE")
-    endif()
-  elseif(MSVC)
-    if(SSE_LEVEL GREATER_EQUAL 4.2)
-      string(APPEND SSE_DEFINITIONS " -D__SSE4_2__")
-    endif()
-    if(SSE_LEVEL GREATER_EQUAL 4.1)
-      string(APPEND SSE_DEFINITIONS " -D__SSE4_1__")
-    endif()
-    if(SSE_LEVEL GREATER_EQUAL 3.1)
-      string(APPEND SSE_DEFINITIONS " -D__SSSE3__")
-    endif()
-    if(SSE_LEVEL GREATER_EQUAL 3.0)
-      string(APPEND SSE_DEFINITIONS " -D__SSE3__")
-    endif()
-    if(SSE_LEVEL GREATER_EQUAL 2.0)
-      string(APPEND SSE_DEFINITIONS " -D__SSE2__")
-    endif()
-    if(SSE_LEVEL GREATER_EQUAL 1.0)
-      string(APPEND SSE_DEFINITIONS " -D__SSE__")
-    endif()
-  endif()
+        IF (HAVE_SSE_EXTENSIONS)
+            SET(SSE_LEVEL 1.0)
+        ENDIF ()
+    ENDIF ()
 
-  set(SSE_FLAGS ${SSE_FLAGS} PARENT_SCOPE)
-  set(SSE_DEFINITIONS ${SSE_DEFINITIONS} PARENT_SCOPE)
+    SET(CMAKE_REQUIRED_FLAGS)
 
-  unset(CMAKE_REQUIRED_FLAGS)
-endfunction()
+    IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+        IF (HAVE_AVX2)
+            STRING(APPEND SSE_FLAGS " -mavx2")
+        ELSEIF (HAVE_AVX)
+            STRING(APPEND SSE_FLAGS " -mavx")
+        ENDIF ()
+        IF (SSE_LEVEL GREATER_EQUAL 1.0)
+            IF (SSE_LEVEL GREATER_EQUAL 4.2)
+                STRING(APPEND SSE_FLAGS " -msse4.2")
+            ELSEIF (SSE_LEVEL GREATER_EQUAL 4.1)
+                STRING(APPEND SSE_FLAGS " -msse4.1")
+            ELSEIF (SSE_LEVEL GREATER_EQUAL 3.1)
+                STRING(APPEND SSE_FLAGS " -msse3")
+            ELSEIF (SSE_LEVEL GREATER_EQUAL 3.0)
+                STRING(APPEND SSE_FLAGS " -msse3")
+            ELSEIF (SSE_LEVEL GREATER_EQUAL 2.0)
+                STRING(APPEND SSE_FLAGS " -msse2")
+            ELSE ()
+                STRING(APPEND SSE_FLAGS " -msse")
+            ENDIF ()
+            STRING(APPEND SSE_FLAGS " -mfpmath=sse")
+        ELSE ()
+            # Setting -ffloat-store to alleviate 32bit vs 64bit discrepancies on non-SSE
+            # platforms.
+            STRING(APPEND SSE_FLAGS " -ffloat-store")
+        ENDIF ()
+
+        IF (PCL_ENABLE_MARCHNATIVE AND (NOT CMAKE_CROSSCOMPILING))
+            IF (HAVE_MARCH)
+                STRING(APPEND SSE_FLAGS " -march=native")
+            ELSE ()
+                STRING(APPEND SSE_FLAGS " -mtune=native")
+            ENDIF ()
+            MESSAGE(STATUS "Using CPU native flags for SSE optimization: ${SSE_FLAGS}")
+        ENDIF ()
+    ELSEIF (MSVC)
+        # Setting the /arch defines __AVX(2)__, see here https://docs.microsoft.com/en-us/cpp/build/reference/arch-x64?view=msvc-160
+        # AVX2 extends and includes AVX.
+        # Setting these defines allows the compiler to use AVX instructions as well as code guarded with the defines.
+        # TODO: Add AVX512 variant if needed.
+        IF (HAVE_AVX2)
+            STRING(APPEND SSE_FLAGS " /arch:AVX2")
+        ELSEIF (HAVE_AVX)
+            STRING(APPEND SSE_FLAGS " /arch:AVX")
+        ENDIF ()
+        IF (SSE_LEVEL GREATER_EQUAL 2.0)
+            SET(SSE_FLAGS " /arch:SSE2")
+        ELSEIF (SSE_LEVEL GREATER_EQUAL 1.0)
+            SET(SSE_FLAGS " /arch:SSE")
+        ENDIF ()
+    ENDIF ()
+    IF (HAVE_AVX2)
+        STRING(APPEND SSE_DEFINITIONS " -D__AVX2__")
+    ELSEIF (HAVE_AVX)
+        STRING(APPEND SSE_DEFINITIONS " -D__AVX__")
+    ENDIF ()
+    IF (SSE_LEVEL GREATER_EQUAL 4.2)
+        STRING(APPEND SSE_DEFINITIONS " -D__SSE4_2__")
+    ENDIF ()
+    IF (SSE_LEVEL GREATER_EQUAL 4.1)
+        STRING(APPEND SSE_DEFINITIONS " -D__SSE4_1__")
+    ENDIF ()
+    IF (SSE_LEVEL GREATER_EQUAL 3.1)
+        STRING(APPEND SSE_DEFINITIONS " -D__SSSE3__")
+    ENDIF ()
+    IF (SSE_LEVEL GREATER_EQUAL 3.0)
+        STRING(APPEND SSE_DEFINITIONS " -D__SSE3__")
+    ENDIF ()
+    IF (SSE_LEVEL GREATER_EQUAL 2.0)
+        STRING(APPEND SSE_DEFINITIONS " -D__SSE2__")
+    ENDIF ()
+    IF (SSE_LEVEL GREATER_EQUAL 1.0)
+        STRING(APPEND SSE_DEFINITIONS " -D__SSE__")
+    ENDIF ()
+    SET(SSE_FLAGS ${SSE_FLAGS} PARENT_SCOPE)
+    SET(SSE_DEFINITIONS ${SSE_DEFINITIONS} PARENT_SCOPE)
+    UNSET(CMAKE_REQUIRED_FLAGS)
+ENDFUNCTION()
